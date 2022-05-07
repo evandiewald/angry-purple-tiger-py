26385bbb94ff5d92e2a486b569bda6221a7e2192
Evan Diewald <evandiewald@gmail.com>
2022-05-07 13:44:07 -0400

    type hints and comments

--- angry_purple_tiger/angry_purple_tiger.py
+++ angry_purple_tiger/angry_purple_tiger.py
@@ -6,14 +6,22 @@ from .compress import compress
 import re
 
 
-def animal_hash(input: bytes):
+def animal_hash(input: bytes) -> str:
+    """
+    Generate the anthropomorphic name given some input bytes.
+    :param input: The input bytes (strings must be encoded).
+    :return: The animal name in angry-purple-tiger format.
+    """
+    # take the md5 checksum of the input bytes
     hex_digest = md5(input).hexdigest()
 
-    p = re.compile('(..?)', )
+    # split the digest into byte pairs and convert to hex ints
+    p = re.compile('(..?)')
     pairs = p.findall(hex_digest)
     bytes = [int(x, base=16) for x in pairs]
     compressed = compress(bytes, 3)
 
+    # use the compressed output as the indices for the adjective-color-animal format
     adjective = adjectives[compressed[0]]
     color = colors[compressed[1]]
     animal = animals[compressed[2]]
--- angry_purple_tiger/compress.py
+++ angry_purple_tiger/compress.py
@@ -1,22 +1,30 @@
 from math import floor
 from functools import reduce
+from typing import List
 
 
-def compress(bytes: list, target: int):
+def compress(bytes: List[int], target: int = 3) -> List[int]:
+    """
+    Compress a list of ints into (usually) 3 indices.
+    :param bytes: The list of input ints.
+    :param target: The size of the compressed output (default: 3).
+    :return: The list of indices of the target length.
+    """
     length = len(bytes)
     if target > length:
         raise ValueError('Fewer input bytes than requested output')
 
+    # split the input list into target number of sublists
     seg_size = floor(length / target)
-
     segments = []
     for i in range(0, seg_size * target, seg_size):
         segments.append(bytes[i:i+seg_size])
 
+    # ensure that any overflow goes into the last segment
     last_seg = segments[len(segments) - 1]
-
     segments[len(segments) - 1] = last_seg + bytes[target * seg_size:]
 
+    # XOR filter to generate the indices
     checksums = [reduce(lambda acc, curr: acc ^ curr, s) for s in segments]
 
     return checksums
